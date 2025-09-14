c16cc1d4c353e159218d2dd1c15e78d72160be5f
Hammad Farooq <hammadf2002@gmail.com>
2025-09-14 16:26:44 +0500

    Addeed the new problems solved

---
+++ is_subsequence.py
@@ -0,0 +1,40 @@
+def isSubsequence(s, t):
+    if s=="":
+        return True
+    i=j=0
+    first=""
+    while i <len(s):
+        first=s[i]
+        while j < len(t):
+            if first==t[j]:
+                # print(first)
+                # print("yea")
+                if i==len(s)-1:
+                    return True
+                j+=1
+                break
+            else:
+                j+=1
+        i+=1
+        if i<=len(s)-1:
+            first=s[i]
+        # print(first)
+        # print(j)
+        # print(t[j])
+        print(i)
+        if j>=len(t)-1:
+            if i < len(s)-1:
+                return False
+            elif j <=len(t)-1:
+                if t[j]==first:
+                    return True
+            print("IOIAOI")
+            return False
+    return False
+# print(isSubsequence( s = "abc", t = "ahbgdc"))
+# print(isSubsequence("a","a"))
+# print(isSubsequence("axc","ahbgdc"))
+# print(isSubsequence("abc", "ahbgdc"))
+# print(isSubsequence("bcd","uuuuuuuuuuuuuuuuuuuuuuuuuuuuuu"))
+print(isSubsequence("bcd","uuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuubcd"))
+print(isSubsequence("aaaaaa","bbaaaa"))
